feed6c3a84802161d6ff9f78d6ed6bef9ef71b81
Blopvisje <122538086+Blopvisje@users.noreply.github.com>
2023-03-09 14:14:27 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let knuffel = 0
+let voeden = 0
+let dood = 0
+basic.forever(function () {
+    knuffel = 20
+    voeden = 20
+    dood = 0
+    if (dood == 0) {
+    	
+    }
+    if (true) {
+    	
+    }
+})
