092f5ebd8ecfa418b6d21abe373b7f48968e419a
Blopvisje <122538086+Blopvisje@users.noreply.github.com>
2023-03-16 14:16:11 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,56 @@
-let knuffel = 0
-let voeden = 0
+let knuffel = 20
+let voeden = 20
 let dood = 0
 basic.forever(function () {
-    knuffel = 20
-    voeden = 20
-    dood = 0
-    if (dood == 0) {
-    	
+    if (voeden <= 0 && knuffel <= 0) {
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
     }
-    if (true) {
-    	
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    if (voeden > 10 && knuffel > 10) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
     }
+    if (dood == 1) {
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    }
+    if (voeden > 0 && voeden <= 10 && (knuffel > 0 && knuffel <= 10)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+    knuffel += -1
+    voeden += -1
+    basic.pause(1000)
+})
+basic.forever(function () {
+	
 })
