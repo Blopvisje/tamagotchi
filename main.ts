1f762e35caef69c434ce08bf737cf24d80cac5b4
Blopvisje <122538086+Blopvisje@users.noreply.github.com>
2023-04-18 15:59:21 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
-let knuffel = 20
-let voeden = 20
 let dood = 0
+let voeden = 20
+let knuffel = 20
 basic.forever(function () {
-    if (voeden <= 0 && knuffel <= 0) {
+    if (dood == 0 && input.buttonIsPressed(Button.A)) {
+        voeden += 5
+    }
+    if (dood == 0 && input.buttonIsPressed(Button.B)) {
+        knuffel += 5
+    }
+    if (dood == 1 && input.buttonIsPressed(Button.A)) {
+        knuffel += 0
+    }
+    if (dood == 1 && input.buttonIsPressed(Button.B)) {
+        voeden += 0
+    }
+})
+basic.forever(function () {
+    knuffel += -1
+    voeden += -1
+    basic.pause(1000)
+})
+basic.forever(function () {
+    if (voeden <= 0 || knuffel <= 0) {
         basic.showLeds(`
             . . . . .
             # # . # #
@@ -10,14 +29,8 @@ basic.forever(function () {
             # # # # #
             . . . . .
             `)
+        dood = 1
     }
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
     if (voeden > 10 && knuffel > 10) {
         basic.showLeds(`
             . . . . .
@@ -27,16 +40,7 @@ basic.forever(function () {
             . # # # .
             `)
     }
-    if (dood == 1) {
-        basic.showLeds(`
-            . . . . .
-            # # . # #
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
-    }
-    if (voeden > 0 && voeden <= 10 && (knuffel > 0 && knuffel <= 10)) {
+    if (voeden > 0 && voeden <= 10 || knuffel > 0 && knuffel <= 10) {
         basic.showLeds(`
             . . . . .
             . # . # .
@@ -45,11 +49,6 @@ basic.forever(function () {
             # . . . #
             `)
     }
-})
-basic.forever(function () {
-    knuffel += -1
-    voeden += -1
-    basic.pause(1000)
 })
 basic.forever(function () {
 	
